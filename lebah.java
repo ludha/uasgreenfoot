820c9adf3f1c3fac4bc8b677dcb7d10fb341fca9
ludha <110885837+ludha@users.noreply.github.com>
2022-08-09 15:24:31 +0700

    Add files via upload
    mengkoneksikan Greenfoot ke database MySql

---
+++ lebah.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.sql.*;
+/**
+ * Write a description of class lebah here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class lebah extends Actor
+{
+    /**
+     * Act - do whatever the lebah wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        {
+        if(Greenfoot.isKeyDown("w")){
+            setLocation(getX(),getY()-2);
+        }
+        if(Greenfoot.isKeyDown("s")){
+            setLocation(getX(),getY()+2);
+        }
+        if(Greenfoot.isKeyDown("a")){
+            setLocation(getX()-2,getY());
+        }
+        if(Greenfoot.isKeyDown("d")){
+            setLocation(getX()+2,getY());
+        }
+    }
+}
+}
